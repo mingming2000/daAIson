ce80d766655acc7e29be4fe7680c5095c795cb0b
Minkyeong Jeon <102716945+mingming2000@users.noreply.github.com>
2023-08-07 12:38:28 +0900

    Delete tests directory

--- tests/test_record.py
+++
@@ -1,9 +0,0 @@
-from blindbee import record
-from blindbee import stt
-
-if __name__ == '__main__':
-    r = record.Record()
-    r.recording("temp_input_audio.linear16")
-
-    s = stt.SpeechToText()
-    test_stt = s.transcribe_audio_to_text("temp_input_audio.linear16")
--- tests/test_stt.py
+++
@@ -1,8 +0,0 @@
-from blindbee import stt
-
-if __name__ == '__main__':
-    test_stt = stt.SpeechToText()
-    test_stt.testing()
-
-    # txt = test_stt.transcribe_audio_to_text("/home/dspi/daAIson/tests/temp_input_audio.linear16")
-    # print(f"input text is : {txt}")
